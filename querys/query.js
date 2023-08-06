daac3216639c3a71bd459da20cbce37ab25e0e36
Bruno Masello <bumasello@gmail.com>
2023-08-06 18:56:14 -0300

    incremento nos updates, geraInvalidoCor rodando

--- querys/query.js
+++ querys/query.js
@@ -1,19 +1,51 @@
 const q_populaBrzCorCursorDtl = `
 BEGIN
-populabrzcorcursorvermelhodtl;
-populabrzcorcursorazuldtl;
-populabrzcorcursorpretodtl;
-populabrzcorcursorrosadtl;
+popula_brz_vermelho_dtl;
+popula_brz_azul_dtl;
+popula_brz_preto_dtl;
+popula_brz_rosa_dtl;
+END;
+`;
+
+const q_updateBrzCorCursorDtl = `
+BEGIN
+update_brz_azul_dtl;
+update_brz_preto_dtl;
+update_brz_rosa_dtl;
+update_brz_vermelho_dtl;
 END;
 `;
 
 const q_populaPrtCor = `
 BEGIN
-popula_prt_pes_doc_pad();
-popula_prt_pes();
-popula_prt_pes_ctt_elet();
-popula_prt_pes_end();
-popula_prt_pes_tel();
+popula_prt_cod_pk;
+popula_prt_pes_doc_pad;
+popula_prt_pes;
+popula_prt_pes_ctt_elet;
+popula_prt_pes_end;
+popula_prt_pes_tel;
+END;
+`;
+
+const q_updatePrtCor = `
+BEGIN
+update_prt_cod_pk;
+update_prt_pes_doc_pad;
+update_prt_pes;
+update_prt_pes_ctt_elet;
+update_prt_pes_end;
+update_prt_pes_tel;
+END;
+`;
+
+const q_geraInvalidoCor = `
+BEGIN
+popula_cura_inv;
+processar_motiv_inv_cnv;
+processar_motiv_inv_qci;
+processar_motiv_inv_qni;
+processar_motiv_inv_ndb;
+processar_motiv_inv_dci;
 END;
 `;
 
@@ -34,7 +66,10 @@ END;
 
 export default {
   q_populaBrzCorCursorDtl,
+  q_updateBrzCorCursorDtl,
   q_populaPrtCor,
+  q_updatePrtCor,
+  q_geraInvalidoCor,
   q_log_controle_inicio,
   q_log_controle_fim,
 };
--- repository/mdmRepository.js
+++ repository/mdmRepository.js
@@ -25,6 +25,30 @@ async function populaBrzCor() {
   }
 }
 
+async function updateBrzCor() {
+  try {
+    // Cria conexão
+    const connection = await oracledb.getConnection({
+      user: "SYSTEM",
+      password: "brumas1027",
+      connectString: "localhost:1521/xe",
+    });
+    console.log("Conectado");
+    // Consulta
+    console.log("Iniciando query");
+    await connection.execute(query.q_log_controle_inicio("updateBrzCor"));
+    console.log("Logado - Inicio");
+    await connection.execute(query.q_updateBrzCorCursorDtl);
+    console.log("Query finalizada...");
+    await connection.execute(query.q_log_controle_fim("updateBrzCor"));
+    console.log("Logado - Fim");
+    await connection.close();
+    console.log("Conexão fechada");
+  } catch (error) {
+    console.error("Erro ao Atualizar Cores", error);
+  }
+}
+
 async function populaPrtCor() {
   try {
     // Cria conexão
@@ -49,6 +73,30 @@ async function populaPrtCor() {
   }
 }
 
+async function updatePrtCor() {
+  try {
+    // Cria conexão
+    const connection = await oracledb.getConnection({
+      user: "SYSTEM",
+      password: "brumas1027",
+      connectString: "localhost:1521/xe",
+    });
+    // Query
+    console.log("Iniciando query");
+    await connection.execute(query.q_log_controle_inicio("updatePrtCor"));
+    console.log("Logado - Inicio");
+    await connection.execute(query.q_updatePrtCor);
+    console.log("Query finalizada...");
+    await connection.execute(query.q_log_controle_fim("updatePrtCor"));
+    console.log("Logado - Fim");
+    // Fecha conexão
+    await connection.close();
+    console.log("Conexão fechada");
+  } catch (error) {
+    console.error("Erro ao Atualizar PRT", error);
+  }
+}
+
 async function geraInvalidoCor() {
   try {
     // Cria conexão
@@ -58,9 +106,16 @@ async function geraInvalidoCor() {
       connectString: "localhost:1521/xe",
     });
     // Query
+    console.log("Iniciando query");
+    await connection.execute(query.q_log_controle_inicio("geraInvalidoCor"));
+    console.log("Logado - Inicio");
     await connection.execute(query); // VERIFICAÇÃO NOME E CPF
+    console.log("Query Finalizada...");
+    await connection.execute(query.q_log_controle_fim("geraInvalidoCor"));
+    console.log("Logado - Fim");
     // Fecha conexão
     await connection.close();
+    console.log("Conexão fechada");
   } catch (error) {
     console.error("Erro ao Gerar Invalido", error);
   }
@@ -124,7 +179,9 @@ async function testConexao() {
 
 export default {
   populaBrzCor,
+  updateBrzCor,
   populaPrtCor,
+  updatePrtCor,
   geraInvalidoCor,
   populaGldCor,
   reproStatusCor,
